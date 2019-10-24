ef10ff7d61ed5d74f2fc2140a3a246ec0a4358b0
zhaojining <jiningzhao@newbanker.cn>
2019-10-24 15:39:03 +0800

    token_access

--- login.py
+++ login.py
@@ -3,115 +3,127 @@
 脚本功能：接口登录
 时间：
 '''
+# encoding: utf-8
+import hashlib
+from urllib.parse import quote
+import requests
+import time
+
 class login():
-    def __init__(self):
-        pass
+    def __init__(self,param,secret):
+        # 拿到secret的值，用来生成sign签名
+        self.secret = secret
+        self.param = self.param_fix(param)
 
-    def md5(self,data): # md5加密算法，且加密后的字符串全部大写——>针对于密码、sign值（拼接之后）的加密
-        pass
 
-    def sign(self): # 根据一定拼接方式，生成sign值——>针对于传参中sign字符串的拼接
-        pass
 
-    def urlEncoding(self): # url编码——>针对于传参中data的url编码
-        pass
+
+
+    def param_fix(self,param):
+
+        # 1.拿到data的值
+        data = param['data']
+
+        # 2.对data值中的password进行md5加密
+        if 'password' in data:
+            password = self.md5(data['password'])
+
+        # 3.加密后的password传回data中
+            data['password'] = password
+
+        # 4.对data值进行字符串处理、字符化处理并进行url编码
+
+        param['data'] = self.urlEncoding(data)
+
+        # 5.调用签名算法生成签名sign，并把sign赋给param，入参param处理完成
+        param['sign'] = self.sign(param)
+
+        return param
+
+
+    # md5加密算法，且加密后的字符串全部大写——>针对于密码、sign值（拼接之后）的加密
+    def md5(self,data):
+
+        # 1.创建md5对象
+        m = hashlib.md5()
+
+        # 2.生成加密串，hashlib.md5(data)函数中，data参数的类型应该是byte，hash前必须把数据转换成bytes类型
+        m.update(data.encode("utf-8"))
+
+        # 3.返回经过md5加密的字符串，全部大写
+        return m.hexdigest().upper()
+
+    # 签名算法：根据一定拼接方式，生成sign值——>针对于传参中sign字符串的拼接
+    def sign(self,param):
+        # 1.将param中的key拿出并生成列表
+        param_keys = list(param.keys())
+
+        # 2.将param_keys列表升序排序
+        param_keys.sort()
+
+        # 3.定义一个空字符串等待拼接
+        string_sign = ''
+
+        # 4.将升序排序的param_keys列表递归
+        for param_key in param_keys:
+
+            # 按照key+value的样式拼接字符串
+            string_sign += param_key+param[param_key]
+
+        # 5.将secret拼接到首位两端
+        string_sign = self.secret + string_sign +self.secret
+
+        # 6.对结果进行md5加密，生成最后的签名
+        return self.md5(string_sign)
+
+    def urlEncoding(self,data): # url编码——>针对于传参中data的url编码
+        # 1.将data转换为字符串
+        string_data = str(data)
+
+        # 2.对字符串格式进行调整
+        string_data = string_data.replace(' ','').replace("'",'"')
+
+        # 3.把字符串转换为bytes类型
+        string_data = string_data.encode("utf-8")
+
+        # 4.最后进行url编码
+        url_data = quote(string_data)
+
+        return url_data
 
     def getCode(self): # 获取返回值中的code值备用
-        pass
+
+        response = requests.get("https://service-wbs300.newtamp.cn/passport/api", params=self.param)
+        code = response.json().get('value').split("=")[-1]
+        print("code:",code)
+        return code
 
     def getToken(self): # 获取token值备用
-        pass
+        response = requests.get("https://service-wbs300.newtamp.cn/passport/api",params = self.param)
+        token = response.json()['value']['token']
+        print("token:",token)
+        return token
 
 
-    n={
-        "name": "passport.login.security",
-        "version": "",
-        "app_key": "test",
-        "data": "%7B%22account%22%3A%2218888888888%22%2C%22password%22%3A%226846860684F05029ABCCC09A53CD66F1%22%2C%22returnUrl%22%3A%22%22%2C%22captcha%22%3A%22%22%7D",
-        "timestamp": "2019-10-24 00:36:51",
-        "format": "json",
-        "sign": "CB8DF0EBA7CAC9AFECCB0A618F8C4A10"
-    }
+if __name__ == "__main__":
+    secret = "123456"
+    param = {
+            "name": "passport.login.security",
+            "version": "",
+            "app_key": "test",
+            "data": {"account":"18888888888","password":"a111111","returnUrl":"","captcha":""},
+            "timestamp": time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()),
+            "format": "json",
+        }
+    a = login(param,secret)
+    b = a.getCode()
 
-    data={
-        "account":"18888888888",
-        "password":"6846860684F05029ABCCC09A53CD66F1",
-        "returnUrl":"",
-        "captcha":""
-    }
-
-    code_response = {"name":"passport.userinfo.bycode",
-                    "version":"",
-                    "app_key":"test",
-                    "data":"%7B%22code%22%3A%2242dc0a8b-d580-48d4-98d6-ad67ac521187%22%7D",
-                    "timestamp":"2019-10-24 00:59:55",
-                    "format":"json",
-                    "sign":"A1776C18F00F74DB281D951C060A8EE5"}
-    code_request = {
-    "code":0,
-    "msg":"操作成功",
-    "value":{
-        "accessCode":"",
-        "admin":1,
-        "assetsManagement":"100000000万元",
-        "cardEnterpirseAddress":"",
-        "createTime":1483409443000,
-        "createTimeLong":0,
-        "createdBy":null,
-        "customerNum":108,
-        "defaultPassword":0,
-        "depId":null,
-        "deptCode":"",
-        "deptName":"",
-        "dismissionDate":null,
-        "displayCusNum":null,
-        "documentNo":"",
-        "documentNoType":null,
-        "documentNoTypeStr":"",
-        "documentType":null,
-        "education":null,
-        "email":"Jensen@163.com",
-        "empDeptVoList":null,
-        "entId":2099,
-        "enterpirseAddress":"",
-        "enterpirseLogo":"https://fs.newbanker.cc/img/2099/2019/7/9/c852fc0a656e4f4b824424eff47c8b6b.jpeg",
-        "enterpirseName":"北京牛投邦科技咨询有限公司300",
-        "enterpriseIntruduction":"<p>q</p>",
-        "enterpriseQrcodeImg":"https://fs.newbanker.cc/img/2099/2019/6/21/7ba5895b2ede4bae99d0e5bc7245a660.jpg",
-        "external":0,
-        "faId":null,
-        "gender":null,
-        "honors":"NB",
-        "id":1,
-        "identifiedAuth":1,
-        "independent":0,
-        "introduction":"Are you kidding me?",
-        "joinDate":null,
-        "lastLoginIp":"124.126.0.166",
-        "lastLoginTime":1571821435000,
-        "loginConfig":2,
-        "manager":null,
-        "married":null,
-        "mobile":"18888888888",
-        "name":"全局管理员",
-        "name1":"S25jQndIbmU=",
-        "no":"",
-        "outId":"f5b67c95-078f-402b-8570-bc3624efb4a5",
-        "pictureUrl":"https://fs.newbanker.cc/1571283534397__1_Advisor_1571283534397.jpg",
-        "positionDesc":"",
-        "positionId":null,
-        "positionName":"",
-        "shortBy":"牛投邦300",
-        "specialty":"All",
-        "status":1,
-        "style":1,
-        "superAdmin":1,
-        "systemCode":"",
-        "thumbsCount":5,
-        "token":"3d7b123c9-77aa-4b78-a41e-6e64641174c0",
-        "updateTime":1571821435000,
-        "updatedBy":null,
-        "weChatID":"Jensen"
-    }
-}
-
+    param1 = {"name":"passport.userinfo.bycode",
+                        "version":"",
+                        "app_key":"test",
+                        "data":{'code':b},
+                        "timestamp":time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()),
+                        "format":"json"
+                        }
+    c = login(param1,secret)
+    d = c.getToken()
